6cee7b2c0355308d23e23be9750627fa06ae15e4
doomtrip <lohurod1@gmail.com>
2025-11-30 23:36:14 +0500

    module8-task2

--- js/full-photo.js
+++ js/full-photo.js
@@ -17,12 +17,12 @@ let commentsShown = 0;
 const createComment = (comment) => {
   const commentElement = document.createElement('li');
   commentElement.classList.add('social__comment');
-  
+
   commentElement.innerHTML = `
     <img class="social__picture" src="${comment.avatar}" alt="${comment.name}" width="35" height="35">
     <p class="social__text">${comment.message}</p>
   `;
-  
+
   return commentElement;
 };
 
@@ -30,18 +30,18 @@ const createComment = (comment) => {
 const renderComments = () => {
   const commentsPortion = currentComments.slice(commentsShown, commentsShown + COMMENTS_PER_PORTION);
   const fragment = document.createDocumentFragment();
-  
+
   commentsPortion.forEach((comment) => {
     const commentElement = createComment(comment);
     fragment.appendChild(commentElement);
   });
-  
+
   socialComments.appendChild(fragment);
-  
+
   // Обновляем счетчик показанных комментариев
   commentsShown += commentsPortion.length;
   commentCountBlock.innerHTML = `${commentsShown} из <span class="comments-count">${currentComments.length}</span> комментариев`;
-  
+
   // Скрываем кнопку, если все комментарии показаны
   if (commentsShown >= currentComments.length) {
     commentsLoader.classList.add('hidden');
@@ -61,7 +61,7 @@ const closeFullPhoto = () => {
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
   commentsLoader.removeEventListener('click', onCommentsLoaderClick);
-  
+
   // Сбрасываем состояние
   currentComments = [];
   commentsShown = 0;
@@ -83,23 +83,23 @@ const openFullPhoto = (photoData) => {
   likesCount.textContent = photoData.likes;
   commentsCount.textContent = photoData.comments.length;
   socialCaption.textContent = photoData.description;
-  
+
   // Сохраняем комментарии и сбрасываем счетчик
   currentComments = photoData.comments;
   commentsShown = 0;
   socialComments.innerHTML = '';
-  
+
   // Показываем блоки с комментариями (убираем hidden)
   commentCountBlock.classList.remove('hidden');
   commentsLoader.classList.remove('hidden');
-  
+
   // Отрисовываем первую порцию комментариев
   renderComments();
-  
+
   // Показываем окно
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
-  
+
   // Добавляем обработчики
   document.addEventListener('keydown', onDocumentKeydown);
   closeButton.addEventListener('click', closeFullPhoto);
